6417085e8c8298d0db8697e068eb74cba5d82365
Olle Rönn <s2310302@student.hb.se>
2025-04-04 10:48:55 +0200

    2025-04-04

--- WorkshopGithub/Program.cs
+++ WorkshopGithub/Program.cs
@@ -1,2 +1,6 @@
 Console.WriteLine("Hello, Olle!");
 Console.WriteLine("Fins väder idag!");
+
+Console.WriteLine("Vad är de för väder idag:");
+    string input  = Console.ReadLine();
+    Console.WriteLine("Vad skönt med " + input);
